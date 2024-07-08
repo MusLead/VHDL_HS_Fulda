1936d4618fa35d19c1dc47174e48e9a521b8c0e4
fd0000748 <agha-muhammad.aslam@informatik.hs-fulda.de>
2024-07-08 17:50:42 +0000

    chore: Replace Collision_Detection.vhd

--- Project/Ball_Collision/Design_Source/Collision_Detection.vhd
+++ Project/Ball_Collision/Design_Source/Collision_Detection.vhd
@@ -56,10 +56,19 @@ architecture Behavioral of Collision_Detection is
 
         signal hit_wall : std_logic_vector(2 downto 0);
         signal hit_racket_l, hit_racket_r : std_logic_vector(1 downto 0);
-
-        function check_collision (x1 : integer; y1 : integer; h1 : integer; w1 : integer; x2 : integer; y2 : integer; h2 : integer ; w2 : integer) return boolean is
+        
+        
+        --All our objects(Ball, rackets, Walls) are represented by rectangles. This function check_collision checks if two rectangles/objects collide or not. Both rectangles are defined by their position and dimensions.
+        function check_collision (
+        x1 : integer; y1 : integer; h1 : integer; w1 : integer;  --Retangle 1: x1, y1: The coordinates of the top-left corner of the first rectangle (object 1) h1: height  w1: width 
+        x2 : integer; y2 : integer; h2 : integer ; w2 : integer  --Retangle 2: x2, y2: The coordinates of the top-left corner of the second rectangle (object 1) h2: height  w: width
+        ) return boolean is
         begin
-                return ((x1 + w1) >= x2) and (x1 <= (x2 + w2)) and ((y1 + h1) >= y2) and (y1 <= (y2 + h2));
+                return ((x1 + w1) >= x2) --if the right edge of rectangle 1 is to the right of the left edge of rectangle 2. Basically, if the right edges of both objects overlap.
+                and (x1 <= (x2 + w2))    --if the left edge of rectangle 1 is to the left of the right edge of rectangle 2. Basically, if the left edges of both objects overlap.
+                and ((y1 + h1) >= y2)    --if the bottom edge of rectangle 1 is below the top edge of rectangle 2. Basically, if the bottom edges of both objects overlap.
+                and (y1 <= (y2 + h2));   --if the top edge of rectangle 1 is above the bottom edge of rectangle 2. Basically, if the top edges of both objects overlap.
+                --The and between all these conditions ensures that only if all these conditions are true, i.e. Both objects intersect, then the function returns true, else false
         end function;
 begin
 
@@ -72,76 +81,89 @@ begin
                 racket_y2 := to_integer(unsigned(racket_y_pos2_i));
                         
                 if check_collision(racket_left_space, racket_y1, racket_height, racket_length, ball_x, ball_y, ball_length, ball_length) then
-                        -- left racket collides with ball 
-
+                        -- left racket collides with ball. 
+                        -- This snippet checks which segment of the racket is actually hit. hit_racket_l represents the output State of left racket getting hit.
+                        -- Switching between segments is done by incrementing the y coordinate with respect to segment_height. 
                         if check_collision(racket_left_space, racket_y1 + (0 * segment_height), segment_height, racket_length, ball_x, ball_y, ball_length, ball_length) then
-                                hit_racket_l <= "01"; -- 1. Segment
+                                hit_racket_l <= "01"; -- 1. Segment hit, set hit_racket_l to State 01
                         elsif check_collision(racket_left_space, racket_y1 + (1 * segment_height), segment_height, racket_length, ball_x, ball_y, ball_length, ball_length) then
-                                hit_racket_l <= "10"; --
+                                hit_racket_l <= "10"; -- 2. Segment hit, set hit_racket_l to State 10
                         elsif check_collision(racket_left_space, racket_y1 + (2 * segment_height), segment_height, racket_length, ball_x, ball_y, ball_length, ball_length) then
-                                hit_racket_l <= "11"; --
+                                hit_racket_l <= "11"; -- 3. Segment hit, set hit_racket_l to State 11
                         elsif check_collision(racket_left_space, racket_y1 + (3 * segment_height), segment_height, racket_length, ball_x, ball_y, ball_length, ball_length) then
-                                hit_racket_l <= "10"; --
+                                hit_racket_l <= "10"; -- 2. Segment hit, set hit_racket_l to State 10
                         elsif check_collision(racket_left_space, racket_y1 + (4 * segment_height), segment_height, racket_length, ball_x, ball_y, ball_length, ball_length) then
-                                hit_racket_l <= "01"; --
+                                hit_racket_l <= "01"; -- 1. Segment hit, set hit_racket_l to State 01
                         end if;
-
-                        hit_wall <= (others => '0');
+                        
+                        -- Reset the collision signals for the wall and right racket. Because anyway if ball hits left racket, then there
+                        -- are no collisions with wall and right racket. 
+                        hit_wall <= (others => '0'); 
                         hit_racket_r <= (others => '0');
                 
                 elsif check_collision(ball_x, ball_y, ball_length, ball_length, racket_right_space, racket_y2, racket_height, racket_length) then
                         -- right racket collides with ball
-
+                        -- This snippet checks which segment of the racket is actually hit. hit_racket_r represents the output State of right racket getting hit.
+                        -- Switching between segments is done by incrementing the y coordinate with respect to segment_height.
                         if check_collision(ball_x, ball_y, ball_length, ball_length, racket_right_space, racket_y2 + (0 * segment_height), segment_height, racket_length) then
-                                hit_racket_r <= "01"; --
+                                hit_racket_r <= "01"; -- 1. Segment hit, set hit_racket_r to State 01
                         elsif check_collision(ball_x, ball_y, ball_length, ball_length, racket_right_space, racket_y2 + (1 * segment_height), segment_height, racket_length) then
-                                hit_racket_r <= "10"; --
+                                hit_racket_r <= "10"; -- 2. Segment hit, set hit_racket_r to State 10
                         elsif check_collision(ball_x, ball_y, ball_length, ball_length, racket_right_space, racket_y2 + (2 * segment_height), segment_height, racket_length) then
-                                hit_racket_r <= "11"; --
+                                hit_racket_r <= "11"; -- 3. Segment hit, set hit_racket_r to State 11
                         elsif check_collision(ball_x, ball_y, ball_length, ball_length, racket_right_space, racket_y2 + (3 * segment_height), segment_height, racket_length) then
-                                hit_racket_r <= "10"; --
+                                hit_racket_r <= "10"; -- 2. Segment hit, set hit_racket_r to State 10
                         elsif check_collision(ball_x, ball_y, ball_length, ball_length, racket_right_space, racket_y2 + (4 * segment_height), segment_height, racket_length) then
-                                hit_racket_r <= "01"; --
+                                hit_racket_r <= "01"; -- 1. Segment hit, set hit_racket_r to State 01
                         end if;
-
+                        
+                        -- Reset the collision signals for the wall and left racket. Because anyway if ball hits right racket, then there
+                        -- are no collisions with wall and left racket. 
                         hit_wall <= (others => '0');
                         hit_racket_l <= (others => '0');
                 else
-                        -- the ball does not collide with any rackets
+                        -- Only one condition left, i.e The ball collides with wall
+                        -- the ball does not collide with any rackets if it collides with wall, hence reset the collision 
+                        -- signals for both rackets. 
                         hit_racket_l <= (others => '0');
                         hit_racket_r <= (others => '0');
                         
                         -- check if the ball collides with wall
-                        if ball_x = 0 then 
-                                hit_wall <= "110"; -- collides with left wall
-                        elsif (ball_x + ball_length) >= (screen_width - 1) then
-                                hit_wall <= "101"; --
-                        elsif ball_y = 0 then 
-                                hit_wall <= "010"; --
-                        elsif (ball_y + ball_length) >= (screen_height - 1) then 
-                                hit_wall <= "011"; --
+                        if ball_x = 0 then                                      -- Checks if the x-coordinate of the ball is at the leftmost edge of the screen
+                                hit_wall <= "110";                              -- If true, the ball collides with left wall, hence set hit_wall to 110
+                        elsif (ball_x + ball_length) >= (screen_width - 1) then -- Checks if the right edge of the ball (x-coordinate plus ball's length) is at or beyond the rightmost edge of the screen
+                                hit_wall <= "101";                              -- If true, the ball collides with right wall, hence set hit_wall to 101
+                        elsif ball_y = 0 then                                   -- Checks if the y-coordinate of the ball is at the topmost edge of the screen.
+                                hit_wall <= "010";                              -- If true, the ball collides with top wall, hence set hit_wall to 010
+                        elsif (ball_y + ball_length) >= (screen_height - 1) then-- Checks if the bottom edge of the ball (y-coordinate plus ball's length) is at or beyond the bottommost edge of the screen.
+                                hit_wall <= "011";                              -- If true, the ball collides with bottom wall, hence set hit_wall to 011
                         else 
-                                hit_wall <= (others => '0'); -- no collision
+                                hit_wall <= (others => '0');                    -- No collision with any wall, hence set hit_wall to 000
                         end if;     
 
                 end if;
 
         end process collision_proc;
-
+        
+        -- Create Flipflops for registering collision signals 
+        
+        -- Flipflop for storing wall collision
         DFF_N_Wall : entity work.D_FlipFlop_NBits
                 generic map (N => 3)
                 port map (clk => clock_i,
                           rst => reset_i,
                           D => hit_wall,
                           Q => hit_wall_o);
-        
+                          
+        -- Flipflop for storing left racket collision
         DFF_N_Racket_L : entity work.D_FlipFlop_NBits
                 generic map (N => 2)
                 port map (clk => clock_i,
                           rst => reset_i,
                           D => hit_racket_l,
                           Q => hit_racket_l_o);
-        
+                          
+        -- Flipflop for storing right racket collision
         DFF_N_Racket_R : entity work.D_FlipFlop_NBits
                 generic map (N => 2)
                 port map (clk => clock_i,
