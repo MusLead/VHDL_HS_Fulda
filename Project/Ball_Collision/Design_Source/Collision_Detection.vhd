38ec80129abb7574cd24a53f88a7b0a0fa519586
aghaaslam246@gmail.com <aghaaslam246@gmail.com>
2024-07-11 16:21:54 +0200

    refactor: use a object oriented approach for a better reading of the code

--- Project/Ball_Collision/Design_Source/Collision_Detection.vhd
+++ Project/Ball_Collision/Design_Source/Collision_Detection.vhd
@@ -57,24 +57,24 @@ architecture Behavioral of Collision_Detection is
         signal hit_wall : std_logic_vector(2 downto 0);
         signal hit_racket_l, hit_racket_r : std_logic_vector(1 downto 0);
 
+        -- Define a record to represent a rectangle
+        type rectangle is record
+                x : integer;
+                y : integer;
+                width : integer;
+                height : integer;
+        end record;
+
         -- This function check_collision checks if two rectangles collide or not. 
         -- Both rectangles are defined by their top-left corner position and dimensions.
         -- The function checks every edge of both rectangles whether they collide with eachother
-        -- x1, y1: The coordinates, h1: height,  w1: width of 1. Rectangle
-        -- x2, y2: The coordinates, h2: height,  w2: width of 2. Rectangle
         -- return value: true if the rectangles collide, false otherwise
         function check_collision(
-                x1 : integer;
-                y1 : integer;
-                h1 : integer;
-                w1 : integer;
-                x2 : integer;
-                y2 : integer;
-                h2 : integer;
-                w2 : integer
+                rect1 : rectangle;
+                rect2 : rectangle
         ) return boolean is
         begin
-                return ((x1 + w1) >= x2) and (x1 <= (x2 + w2)) and ((y1 + h1) >= y2) and (y1 <= (y2 + h2));
+                return ((rect1.x + rect1.width) >= rect2.x) and (rect1.x <= (rect2.x + rect2.width)) and ((rect1.y + rect1.height) >= rect2.y) and (rect1.y <= (rect2.y + rect2.height));
         end function;
 begin
 
@@ -82,72 +82,98 @@ begin
         -- if collision happen either left or right racket then we want to know which segment of the racket was hit.
         -- Switching between segments is done by incrementing the y coordinate with respect to segment_height.
         collision_proc : process (reset_i, ball_x_i, ball_y_i, racket_y_pos1_i, racket_y_pos2_i)
-                variable ball_x, ball_y, racket_y1, racket_y2 : integer;
+                variable ball, racket_l, racket_r, segment : rectangle;
         begin
-                ball_x := to_integer(unsigned(ball_x_i));
-                ball_y := to_integer(unsigned(ball_y_i));
-                racket_y1 := to_integer(unsigned(racket_y_pos1_i));
-                racket_y2 := to_integer(unsigned(racket_y_pos2_i));
+                -- Initialize the variables
 
-                if check_collision(racket_left_space, racket_y1, racket_height, racket_length, ball_x, ball_y, ball_length, ball_length) then
-                        -- left racket collides with ball. 
-                      
+                ball := (x => to_integer(unsigned(ball_x_i)),
+                         y => to_integer(unsigned(ball_y_i)),
+                         width => ball_length,
+                         height => ball_length);
+
+                racket_l := (x => racket_left_space,
+                             y => to_integer(unsigned(racket_y_pos1_i)),
+                             width => racket_length,
+                             height => racket_height);
+
+                racket_r := (x => racket_right_space,
+                             y => to_integer(unsigned(racket_y_pos2_i)),
+                             width => racket_length,
+                             height => racket_height);
+
+                -- Check if the ball collides with the rackets
+                if check_collision(racket_l, ball) then  -- left racket collides with ball. 
+                
                         hit_racket_l <= (others => '0'); -- this line to remove an unwanted latch
-                        
-                        if check_collision(racket_left_space, racket_y1 + (0 * segment_height), segment_height, racket_length, ball_x, ball_y, ball_length, ball_length) then
-                                hit_racket_l <= "01"; -- 1. Segment hit
-                        elsif check_collision(racket_left_space, racket_y1 + (1 * segment_height), segment_height, racket_length, ball_x, ball_y, ball_length, ball_length) then
-                                hit_racket_l <= "10"; -- 2. Segment hit
-                        elsif check_collision(racket_left_space, racket_y1 + (2 * segment_height), segment_height, racket_length, ball_x, ball_y, ball_length, ball_length) then
-                                hit_racket_l <= "11"; -- 3. Segment hit
-                        elsif check_collision(racket_left_space, racket_y1 + (3 * segment_height), segment_height, racket_length, ball_x, ball_y, ball_length, ball_length) then
-                                hit_racket_l <= "10"; -- 2. Segment hit
-                        elsif check_collision(racket_left_space, racket_y1 + (4 * segment_height), segment_height, racket_length, ball_x, ball_y, ball_length, ball_length) then
-                                hit_racket_l <= "01"; -- 1. Segment hit
-                        end if;
+
+                        -- check which segment of the racket was hit
+                        for i in 0 to 4 loop
+                                segment := (x => racket_left_space, 
+                                            y => racket_l.y + (i * segment_height), 
+                                            width => racket_length, 
+                                            height => segment_height);
+
+                                if check_collision(segment, ball) then
+                                        case i is
+                                                when 0 => hit_racket_l <= "01"; -- 1. Segment hit
+                                                when 1 => hit_racket_l <= "10"; -- 2. Segment hit
+                                                when 2 => hit_racket_l <= "11"; -- 3. Segment hit
+                                                when 3 => hit_racket_l <= "10"; -- 2. Segment hit
+                                                when 4 => hit_racket_l <= "01"; -- 1. Segment hit
+                                                when others => null;
+                                        end case;
+                                        exit;
+                                end if;
+                        end loop;
 
                         hit_wall <= (others => '0');     -- no collision with wall
                         hit_racket_r <= (others => '0'); -- no collision with right racket
 
-                elsif check_collision(ball_x, ball_y, ball_length, ball_length, racket_right_space, racket_y2, racket_height, racket_length) then
-                        -- right racket collides with ball
-                    
+                elsif check_collision(ball, racket_r) then  -- right racket collides with ball
+                       
                         hit_racket_r <= (others => '0'); -- this line to remove an unwanted latch
+                        
+                        -- check which segment of the racket was hit
+                        for i in 0 to 4 loop
+                                segment := (x => racket_right_space, 
+                                            y => racket_r.y + (i * segment_height), 
+                                            width => racket_length, 
+                                            height => segment_height);
 
-                        if check_collision(ball_x, ball_y, ball_length, ball_length, racket_right_space, racket_y2 + (0 * segment_height), segment_height, racket_length) then
-                                hit_racket_r <= "01"; -- 1. Segment hit
-                        elsif check_collision(ball_x, ball_y, ball_length, ball_length, racket_right_space, racket_y2 + (1 * segment_height), segment_height, racket_length) then
-                                hit_racket_r <= "10"; -- 2. Segment hit
-                        elsif check_collision(ball_x, ball_y, ball_length, ball_length, racket_right_space, racket_y2 + (2 * segment_height), segment_height, racket_length) then
-                                hit_racket_r <= "11"; -- 3. Segment hit
-                        elsif check_collision(ball_x, ball_y, ball_length, ball_length, racket_right_space, racket_y2 + (3 * segment_height), segment_height, racket_length) then
-                                hit_racket_r <= "10"; -- 2. Segment hit
-                        elsif check_collision(ball_x, ball_y, ball_length, ball_length, racket_right_space, racket_y2 + (4 * segment_height), segment_height, racket_length) then
-                                hit_racket_r <= "01"; -- 1. Segment hit
-                        end if;
+                                if check_collision(ball, segment) then
+                                        case i is
+                                                when 0 => hit_racket_r <= "01"; -- 1. Segment hit
+                                                when 1 => hit_racket_r <= "10"; -- 2. Segment hit
+                                                when 2 => hit_racket_r <= "11"; -- 3. Segment hit
+                                                when 3 => hit_racket_r <= "10"; -- 2. Segment hit
+                                                when 4 => hit_racket_r <= "01"; -- 1. Segment hit
+                                                when others => null;
+                                        end case;
+                                        exit;
+                                end if;
+                        end loop;
 
                         hit_wall <= (others => '0');     -- no collision with wall
                         hit_racket_l <= (others => '0'); -- no collision with left racket
+                        
                 else
                         -- the ball does not collide with any rackets
                         hit_racket_l <= (others => '0');
                         hit_racket_r <= (others => '0');
 
                         -- check if the ball collides with wall
-                        if ball_x = 0 then
+                        if ball.x = 0 then
                                 hit_wall <= "110"; -- collides with left wall
-                        elsif (ball_x + ball_length) >= (screen_width - 1) then
+                        elsif (ball.x + ball.width) >= (screen_width - 1) then
                                 hit_wall <= "101"; -- collides with right wall
-                        elsif ball_y = 0 then
+                        elsif ball.y = 0 then
                                 hit_wall <= "010"; -- collides with top wall
-                        elsif (ball_y + ball_length) >= (screen_height - 1) then
+                        elsif (ball.y + ball.height) >= (screen_height - 1) then
                                 hit_wall <= "011"; -- collides with bottom wall
                         else
                                 hit_wall <= (others => '0'); -- no collision
                         end if;
-
                 end if;
-
         end process collision_proc;
 
         -- Create Flipflops for registering collision signals 
