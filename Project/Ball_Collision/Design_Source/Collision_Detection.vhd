5446f31543bb25a27f0520907c25709c71bc1146
fd0000748 <agha-muhammad.aslam@informatik.hs-fulda.de>
2024-08-07 22:02:21 +0000

    refactor: Update Collision_Detection.vhd

--- Project/Ball_Collision/Design_Source/Collision_Detection.vhd
+++ Project/Ball_Collision/Design_Source/Collision_Detection.vhd
@@ -74,7 +74,10 @@ architecture Behavioral of Collision_Detection is
                 rect2 : rectangle
         ) return boolean is
         begin
-                return ((rect1.x + rect1.width) >= rect2.x) and (rect1.x <= (rect2.x + rect2.width)) and ((rect1.y + rect1.height) >= rect2.y) and (rect1.y <= (rect2.y + rect2.height));
+                return ((rect1.x + rect1.width) >= rect2.x) and 
+                        (rect1.x <= (rect2.x + rect2.width)) and 
+                        ((rect1.y + rect1.height) >= rect2.y) and 
+                        (rect1.y <= (rect2.y + rect2.height));
         end function;
 begin
 
